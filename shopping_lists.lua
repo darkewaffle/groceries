5dde269b98d59d8ac6eae136673f286c4cdc1bcb
darkwaffle <darkewaffle@gmail.com>
2026-06-26 21:00:01 -0400

    Expanded comments regarding bid amount permitted as either number or string.

--- shopping_lists.lua
+++ shopping_lists.lua
@@ -1,7 +1,7 @@
 -- Lists should take the form of Item Name, Bid Amount, Quantity, Max Purchased.
--- Item Name and Bid Amount are self explanatory. Quantity represents if you are bidding on
--- a single item or a stack. Max Purchased represents how many times Groceries will try to 
--- successfully buy the item.
+-- Item Name and Bid Amount are self explanatory however the Bid Amount can be numeric or a string with comma separators. 
+-- Quantity represents if you are bidding on a single item or a stack.
+-- Max Purchased represents how many times Groceries will try to successfully buy the item.
 -- Quantity and Max Purchased are both optional, but if they are not present then they will
 -- both default to 1. Zero is a valid Max Purchased amount if you want an item to remain in the list
 -- but do not wish to make any bids on it currently.
@@ -15,10 +15,11 @@ ShoppingLists.main =
 {
 	{"Grape Daifuku",              30000,             12,                2},
 	{"Earth Crystal",               3000,             12,                5},
-	{"Earth Cluster",             100000,             12,                1},
+	{"Earth Cluster",             100000,             12,                0},
 	{"Alexandrite",               200000,             99,                3},
 	{"Dark Matter",               750000,              1,                2},
-	{"Tiger King Hide",            50000,              1,                1}
+	{"Tiger King Hide",            50000,              1,                1},
+	{"Hoxne Ampulla",        "25,000,000"}
 }
 
 ShoppingLists.fire =
