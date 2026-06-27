093bc0dae5695f2c2a6ff4ac7ff79b0778d1d9e4
darkwaffle <darkewaffle@gmail.com>
2026-06-26 20:35:45 -0400

    NumberToStringWithCommas function created.

--- libraries/modify_strings.lua
+++ libraries/modify_strings.lua
@@ -7,3 +7,25 @@ function ColorWrapForTexts(StringToWrap, Red, Green, Blue)
 	local ColorSuffix = "\\cr"
 	return ColorPrefix .. StringToWrap .. ColorSuffix
 end
+
+function NumberToStringWithCommas(Input, Output)
+	if type(Input) ~= "string" then
+		Input = tostring(Input)
+	end
+
+	if not Output then
+		Output = ""
+	end
+
+	if #Input >= 4 then
+		local Head = string.sub(Input, 1, #Input - 3)
+		local Tail = string.sub(Input, #Input - 2, #Input)
+
+		Output = "," .. Tail .. Output
+		Output = NumberToStringWithCommas(Head, Output)
+	else
+		Output = Input .. Output
+	end
+
+	return Output
+end
